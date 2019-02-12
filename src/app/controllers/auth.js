41500cc47ac3652badc6be5ee5e3513f70f0eeaa
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-12 01:11:19 -0200

    add auth constroller

---
+++ src/app/controllers/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(params = {}) {
+  return jwt.sign(
+    { params },
+    process.env.JWT_SECRET,
+    { expiresIn: 86400 },
+  );
+}
+
+module.exports = {
+  generateToken,
+};
