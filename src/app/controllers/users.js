21f268c2908441d20b762985fa817b4ac6da5bf3
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-14 09:07:35 -0200

    update user controllers

--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -1,7 +1,13 @@
-/* eslint-disable no-throw-literal */
 /* eslint-disable no-console */
 const User = require('../models/user');
 const { generateToken } = require('./auth');
+
+const PermissionErr = 'User without permission. Token not compatible with user';
+
+function operationError(operation) {
+  if (!operation) return 'Error to do operation';
+  return `Error to ${operation} user.`;
+}
 
 const getAll = async (req, res) => {
   try {
@@ -9,7 +15,7 @@ const getAll = async (req, res) => {
     return res.status(200).send({ users });
   } catch (err) {
     console.error(err);
-    return res.status(400).send({ error: 'Error to find users.' });
+    return res.status(400).send({ error: operationError('find') });
   }
 };
 
@@ -20,7 +26,7 @@ const getById = async (req, res) => {
     return res.status(200).json({ user });
   } catch (err) {
     console.error(err);
-    return res.status(400).send({ error: 'Error to find the user.' });
+    return res.status(400).send({ error: operationError('find') });
   }
 };
 
@@ -35,31 +41,31 @@ const insert = async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    return res.status(400).send({ error: 'Registration failed' });
+    return res.status(400).send({ error: operationError('register') });
   }
 };
 
 const update = async (req, res) => {
   const { id } = req.params;
   try {
-    if (req.userId !== id) throw 'User without permission. Token not compatible with user';
+    if (req.userId !== id) throw new Error(PermissionErr);
     const user = await User.findOneAndUpdate({ _id: id }, req.body);
     return res.status(200).send(user);
   } catch (err) {
     console.error(err);
-    return res.status(400).send({ error: 'Error to update the user.' });
+    return res.status(400).send({ error: operationError('update') });
   }
 };
 
 const remove = async (req, res) => {
   const { id } = req.params;
   try {
-    if (req.userId !== id) throw 'User without permission. Token not compatible with user';
+    if (req.userId !== id) throw new Error(PermissionErr);
     await User.remove({ _id: id });
     return res.status(201).send({ message: 'User removed with success.' });
   } catch (err) {
     console.error(err);
-    return res.status(400).send({ error: 'Error to remove the user.' });
+    return res.status(400).send({ error: operationError('remove') });
   }
 };
 
