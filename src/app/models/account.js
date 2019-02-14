1487174be3ae9e999bd9a26c218822f2eb403199
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-14 09:08:47 -0200

    add account models

---
+++ src/app/models/account.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const max = [99999999, 'The value of path `{PATH}` ({VALUE}) exceeds the limit ({MAX}).'];
+
+const AccountSchema = new Schema({
+  bank: {
+    type: Number,
+    required: true,
+    default: 852,
+  },
+  agency: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  account: {
+    type: Number,
+    required: true,
+    unique: true,
+    max,
+  },
+  balence: {
+    type: Schema.Types.Decimal128,
+    default: 0.0,
+  },
+});
+
+module.exports = mongoose.model('Account', AccountSchema);
