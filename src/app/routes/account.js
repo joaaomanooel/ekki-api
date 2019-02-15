e91e3711505d5316bbaab8e870fdca80d730360c
João Manoel Neto <joaaomanooel@gmail.com>
2019-02-15 20:01:38 -0200

    fix router authenticate

--- src/app/routes/account.js
+++ src/app/routes/account.js
@@ -3,11 +3,10 @@ const controller = require('../controllers/account');
 const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
-router.use(authMiddleware);
-router.get('/account', controller.getAll);
-router.get('/account/:id', controller.getById);
-router.post('/account', controller.insert);
-router.put('/account/:id', controller.update);
-router.delete('/account/:id', controller.remove);
+router.get('/account', authMiddleware, controller.getAll);
+router.get('/account/:id', authMiddleware, controller.getById);
+router.post('/account', authMiddleware, controller.insert);
+router.put('/account/:id', authMiddleware, controller.update);
+router.delete('/account/:id', authMiddleware, controller.remove);
 
 module.exports = router;
